151e476d175cd5042f471aa46581bf03126be9d2
KiwiGin <bayeskiwigen@gmail.com>
2024-03-27 18:52:53 -0500

    Corregir inicio de sesión y agregar manejo de errores

--- views/iniciar/index.php
+++ views/iniciar/index.php
@@ -1,18 +1,20 @@
 <?php
-        while ($row = mysqli_fetch_array($this->tablaSQL ,MYSQLI_ASSOC)) {
-            session_start();
-            $_SESSION['IdUsuario'] = $row['IdUsuario'];
-            $_SESSION['IdRol'] = $row['IdRol'];
-            $_SESSION['Nombres'] = $row['Nombres'];
-            $_SESSION['Apellidos'] = $row['Apellidos'];
-            $_SESSION['Correo'] = $row['Correo'];
-            $_SESSION['Contrasena'] = $row['Contrasena'];
-            $_SESSION['Telefono'] = $row['Telefono'];
-            $_SESSION['imagen'] = !empty($row['imagen']) ? $row['imagen'] : null;
-
-        }
-        ?>
-<!DOCTYPE html>
+        
+        if($row = mysqli_fetch_array($this->tablaSQL ,MYSQLI_ASSOC)){
+            while ($row = mysqli_fetch_array($this->tablaSQL ,MYSQLI_ASSOC)) {
+                session_start();
+                $_SESSION['IdUsuario'] = $row['IdUsuario'];
+                $_SESSION['IdRol'] = $row['IdRol'];
+                $_SESSION['Nombres'] = $row['Nombres'];
+                $_SESSION['Apellidos'] = $row['Apellidos'];
+                $_SESSION['Correo'] = $row['Correo'];
+                $_SESSION['Contrasena'] = $row['Contrasena'];
+                $_SESSION['Telefono'] = $row['Telefono'];
+                $_SESSION['imagen'] = !empty($row['imagen']) ? $row['imagen'] : null;
+    
+            }
+            echo '
+            <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -28,3 +30,25 @@
 
 </body>
 </html>
+            ';
+        }else{
+            echo '
+            <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="content/styles/style.css">
+    <title>Inicio No Exitoso</title>
+</head>
+<body>
+    <script>
+        alert("Inicio No Exitoso");
+        location.href = "main";
+    </script>
+
+</body>
+</html>
+            ';
+        }
+?>
